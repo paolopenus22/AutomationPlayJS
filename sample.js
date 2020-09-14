cd5dd566be6c1594fa9ab9ebeee678d4f5ed90c6
Paolo Ray Penus <Paolo.Ray.M.Penus@ey.com>
2020-09-15 00:45:08 +0800

    Install seleniumwebdriver

--- sample.js
+++ sample.js
@@ -1,1 +1,11 @@
-//sample.js
+const { Builder } = require("selenium-webdriver");
+const chrome = require('chromedriver');
+
+const driver = new Builder().forBrowser('chrome').build();
+
+async function Demo() {
+    await driver.get('https://www.youtube.com/');
+    await driver.close();
+}
+
+Demo();
