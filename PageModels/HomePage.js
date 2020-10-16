d7f6a60708ae73f5b70f35f47c0425ea140f79d7
Anne Macabale <AnneM@magenic.com>
2020-10-16 11:17:22 -0700

    Create Page Models for HomePage and Register Page

---
+++ PageModels/HomePage.js
@@ -0,0 +1,50 @@
+const { By, until } = require("selenium-webdriver");
+const BasePage = require("../PageModels/BasePage");
+
+let moviesTab = By.css('a[href="/movies"]');
+let branchesTab = By.css('a[href="/branches"]');
+let loginTab = By.css('a[href="/login"]');
+let registerTab = By.css('a[href="/register"]');
+let movieList = By.css('div[class="text-center"]:nth-child(2) a');
+let movieTrailerButton = By.css('div[class="text-center"]:nth-child(3) button:first-child');
+let getTicketButton = By.css('div[class="text-center"]:nth-child(3) button:nth-child(2)');
+let movieTimeSlot = By.css('br[_ngcontent-yho-c22]:nth-child(2)');
+
+class HomePage extends BasePage {
+    
+    verifyMoviesTabisDisplayed = async () => {
+        return await this.verifyPageLoad(moviesTab);
+    };
+    verifyBrachesTabisDisplayed = async () => {
+        return await this.verifyPageLoad(branchesTab);
+    };
+    verifyLoginTabisDisplayed = async () => {
+        return await this.verifyPageLoad(loginTab);
+    };
+    verifyRegisterTabisDisplayed = async () => {
+        return await this.verifyPageLoad(registerTab);
+    }
+    clickMoviesTab = async () => {
+        await this.clickElement(moviesTab);
+    }
+    clickBranchesTab = async () => {
+        await this.clickElement(branchesTab);
+    }
+    clickLoginTab = async () => {
+        await this.clickElement(loginTab);
+    }
+    clickRegisterTab = async () => {
+        await this.clickElement(registerTab);
+    }
+    checkMoviesAvailable = async (selectMovie, movieTime) => {
+        let movies = await this.driver.findElements(movieList);
+        let time = await this.driver.findElements(movieTimeSlot);
+        for (let i = 0; i < movies.length; i++) {
+            if (await movies[i].getText() === selectMovie && await time[i].getText() === movieTime) {
+                await movies[i].findElement(getTicketButton).click();
+            }
+        }
+    }
+}
+
+module.exports = HomePage;
--- PageModels/RegisterPage.js
+++ PageModels/RegisterPage.js
@@ -8,6 +8,16 @@ let middleNameField = By.css('input[formcontrolname="middleName"]');
 let lastNameField = By.css('input[formcontrolname="lastName"]');
 let birthdayField = By.css('input[formcontrolname="birthDay"]');
 let registerButton = By.css('button[type="submit"]');
+let bdayIcon = By.css('icon ion-ios-calendar');
+let emailErrorMsg = By.css('span[class="ng-star-inserted"]');
+let passwordErrorMsg = By.css('div[class="ng-star-inserted"]');
+let firstNameErrorMsg = By.css('div.row:nth-child(2) div[class="ng-star-inserted"]');
+let lastNameErrorMsg = By.css('div.row:nth-child(3) div[class="ng-star-inserted"]');
+let bdayErrorMsg = By.css('div.row:nth-child(3) div:nth-child(2) div[class="ng-star-inserted"]');
+let loginButton = By.css('a[href="/login"]');
+let birthMonthDP = By.css('select:first-child option[class="ng-star-inserted"]');
+let birthYearDP = By.css('select:nth-child(2) option[class="ng-star-inserted"]');
+let birthDayDP = By.css('div[class="btn-light ng-star-inserted"]');
 
 class RegisterPage extends BasePage {
     
@@ -19,6 +29,57 @@ class RegisterPage extends BasePage {
         await this.enterText(lastNameField, lastName);
         await this.enterText(birthdayField, birthday);
         await this.clickElement(registerButton);
+    }
+    getEmailErrorMessage = async () => {
+        return await this.getText(emailErrorMsg);
+    }
+    getPasswordErrorMessage = async () => {
+        return await this.getText(passwordErrorMsg);
+    }
+    getFirstNameErrorMessage = async () => {
+        return await this.getText(firstNameErrorMsg);
+    }
+    getLastNameErrorMessage = async () => {
+        return await this.getText(lastNameErrorMsg);
+    }
+    getBdayErrorMessage = async () => {
+        return await this.getText(bdayErrorMsg);
+    }
+    clickBirthdayDatePickerIcon = async () => {
+        await this.clickElement(bdayIcon);
+    }
+    selectBirthMonth = async (birthMonth) => {
+        let month = await this.driver.findElements(birthMonthDP);
+        for(let i = 0; i < month.length; i++) {
+            if (await month[i].getText() === birthMonth) {
+                await month[i].click();
+            }
+        }
+    }
+    selectBirthYear = async (birthYear) => {
+        let year = await this.driver.findElements(birthYearDP);
+        for (let i = 0; i < year.length; i++) {
+            if (await year[i].getText() === birthYear) {
+                await year[i].click();
+            }
+        }
+    }
+    selectBirthDay = async (birthDay) => {
+        let day = await this.driver.findElements(birthDayDP);
+        for (let i = 0; i < year.length; i++) {
+            if (await day[i].getText() === birthDay) {
+                await day[i].click();
+            }
+        }
+    }
+    verifyRegisterButtonDisabled = async () => {
+        return await this.verifyElementEnabled(registerButton);
+    }
+    clickLoginMemberAccount = async () => {
+        await this.clickElement(loginButton);
+    }
+    isPageLoaded = async () => {
+        return await this.verifyPageLoad(firstNameField);
     }
 }
 
