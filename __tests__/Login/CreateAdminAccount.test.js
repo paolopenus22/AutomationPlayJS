62713edc581cd029b29efbbad9a9de08b5c65f77
Paolo Ray Penus <Paolo.Ray.M.Penus@ey.com>
2020-11-06 01:35:01 +0800

    Refactor

--- __tests__/Login/CreateAdminAccount.test.js
+++ __tests__/Login/CreateAdminAccount.test.js
@@ -2,27 +2,28 @@ let LandingPage = require('../../PageModels/LandingPage');
 let RegisterPage = require('../../PageModels/RegisterPage');
 let LoginPage = require('../../PageModels/LoginPage');
 let HomePage = require('../../PageModels/HomePage');
+let Utils = require('../../utils/cleanup');
 
 describe('Create an admin account', () => {
+
+    let adminEmail = 'testqa41@admin.com';
+    let adminPassword = '@dmin';
+    let adminFirstName = 'Anne';
+    let adminMiddleName = 'Gonzales';
+    let adminLastName = 'Macabale';
+    let adminBday = '1994-10-11';
 
     beforeEach(async () => {
         this.landingPage = new LandingPage();
         this.registerPage = new RegisterPage();
         this.loginPage = new LoginPage();
         this.homePage = new HomePage();
+        this.cleanUp = new Utils();
         jest.setTimeout(40000);
         await this.landingPage.navigateToMoviesApp();
     });
 
     test('Create admin account and verify login', async () => {
-        
-        let adminEmail = 'testqa28@admin.com';
-        let adminPassword = '@dmin';
-        let adminFirstName = 'Anne';
-        let adminMiddleName = 'Gonzales';
-        let adminLastName = 'Macabale';
-        let adminBday = '1994-10-11';
-
         await this.landingPage.clickRegisterButton();
         await this.registerPage.inputUserDetails(adminEmail, adminPassword, adminFirstName, adminMiddleName, adminLastName, adminBday);
         await this.registerPage.clickRegisterButton();
@@ -32,10 +33,11 @@ describe('Create an admin account', () => {
         expect(await this.homePage.verifyBrachesTabisDisplayed()).toBe(true);
         expect(await this.homePage.verifyAdminTabisDisplayed()).toBe(true);
         expect(await this.homePage.verifyUserLoggedin()).toEqual(adminEmail);
-        expect(await this.homePage.verifyLogoutButtonisDisplayed()).toEqual('Logout');
+        expect(await this.homePage.verifyLogoutButtonisDisplayed()).toEqual('Logout'); 
     });
 
     afterAll(async () => {
         await this.landingPage.closeMoviesApp();
+        await this.cleanUp.deleteUser(adminEmail);
     });
 });
--- utils/cleanup.js
+++ utils/cleanup.js
@@ -1,19 +1,54 @@
-const axios = require('axios');
-const url = process.env.BASE_API_URL || 'http://magenicmovies-api.azurewebsites.net/';
+const axios = require('axios').default;
+const url = 'https://magenicmovies-api.azurewebsites.net/';
 
 class CleanUp {
     
-    deleteUser = async (id) => {
+    getUserID = async (emailAdd) => {
+        try 
+        {
+            const response = await axios.get(`${url}users?email=${emailAdd}`);
+            return response.data[0]["id"];
+        } 
+        catch (e) 
+        {
+            console.log("Email not existing" + e);
+        }
+    }
+    getCinemaID = async (name) => {
+        try 
+        {
+            const response = await axios.get(`${url}cinemas?name=${name}`);
+            return response.data[0]["id"];
+        } 
+        catch (e) 
+        {
+            console.log("Cinema not existing" + e);
+        }
+    }
+    getBranchID = async (branch) => {
+        try 
+        {
+            const response = await axios.get(`${url}branches?name=${branch}`);
+            return response.data[0]["id"];
+        } 
+        catch (e) 
+        {
+            console.log("Branch not existing" + e);
+        }
+    }
+    deleteUser = async (emailAdd) => {
+        let id = await this.getUserID(emailAdd);
         try 
         {
             await axios.delete(`${url}users/${id}`);
         } 
         catch (e) 
         {
-            console.log("Cannot delete user " + id + + " " + e);
+            console.log("Cannot delete user " + emailAdd + + " " + e);
         }
     }
-    deleteBranch = async (id) => {
+    deleteBranch = async (branch) => {
+        let id = await this.getBranchID(branch);
         try 
         {
             await axios.delete(`${url}branches/${id}`);
@@ -23,7 +58,8 @@ class CleanUp {
             console.log("Cannot delete branch " + id + + " " + e);
         }
     }
-    deleteCinema = async (id) => {
+    deleteCinema = async (cinema) => {
+        let id = await this.getCinemaID(cinema);
         try 
         {
             await axios.delete(`${url}cinemas/${id}`);
